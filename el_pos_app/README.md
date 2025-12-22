35156b23539a31e5493d753d91ed2101cb9a24f9
Jonah St <jonahst10@gmail.com>
2025-12-22 14:04:04 -0800

    Initialized empty flutter project, updated Flutter on my machine and confirmed an Android 36 VM runs. Ready for when I need it later.

---
+++ el_pos_app/README.md
@@ -0,0 +1,3 @@
+# el_pos_app
+
+A new Flutter project.
---
+++ el_pos_app/android/app/src/main/kotlin/com/example/el_pos_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.el_pos_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
